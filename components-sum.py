f5de30b7cba2a65b52f1cceab59e0e5585699513
Rodrigo <camachorod@gmail.com>
2016-01-24 00:59:55 +0100

    removed useless imports

--- components-sum.py
+++ components-sum.py
@@ -1,9 +1,10 @@
 #------------------------
 #This program finds the components of a number in terms of addition. 
 #"sums" are the sums that you are looking for and the "components" are the potential pieces of one of those sums.
+#This version allows for any component to be used by more than one sum ==> inverse also true
 #------------------------
 
-import random, itertools, timeit
+import itertools
 
 
 #PROBLEM INFORMATION INPUT
